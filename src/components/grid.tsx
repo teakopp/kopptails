269c98fdef3346485a6eeeed0319d3ea610c454b
teakopp <teakopp@github.com>
2023-02-03 03:17:14 -0800

    adding comments

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -18,11 +18,13 @@ class Grid extends React.Component<GridProps, GridState> {
     super(props);
     this.state = {
       gridData: [
-        { id: "0", src: "", title: "Test"},
+        { id: "", src: "", title: ""},
       ],
     };
   }
 
+  // Gets 6 random drinks then loads results into stateData to
+  // render it
   getRandomDrinks = async () => {
     const data = [];
     // Picked number 6 because it seemed like that would be enough to fill page
@@ -39,11 +41,17 @@ class Grid extends React.Component<GridProps, GridState> {
     this.setState({ gridData: data });
   };
 
+  // A general function created to fetch data should any of the page statuses change
+  // pageStatus wasn't the best choice for var names as it's not as desciptive as it should
+  // be and could stand to be changed later
   updateData = async () => {
     if (this.props.pageStatus === "Suprise Me") {
       this.getRandomDrinks();
     }
-
+    
+    // These functions are all very similar. The only reason they aren't 
+    // using the same function to unpack data is because typescript can make that
+    // a bit time consuming when dealing with multiple data return types
     if (this.props.pageStatus === "By Category") {
       const filter = this.props.filterStatus || "Ordinary Drink"
       const res = await getDrinksByCategory(filter);
@@ -86,15 +94,14 @@ class Grid extends React.Component<GridProps, GridState> {
       }
     }
   }
+  
 
-  async componentDidMount() {
-    await this.updateData();
-  }
+// This updates component when props have been updated
+// Instead of just waiting for state update to render
+// This is helpful because there are two states being passed in as
+// props that are updated with an outside function
 componentDidUpdate(prevProps: GridProps) {
-    if (prevProps.pageStatus !== this.props.pageStatus) {
-      this.updateData();
-    }
-    if (prevProps.filterStatus!== this.props.filterStatus) {
+    if (prevProps.pageStatus !== this.props.pageStatus || prevProps.filterStatus!== this.props.filterStatus) {
       this.updateData();
     }
   }
