3314f187c9ab25a0d92308dfa2d2bdde5a6908cb
teakopp <teakopp@github.com>
2023-02-03 19:29:38 -0800

    add more tests

---
+++ src/components/__tests__/sublist.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { cleanup, render, screen, fireEvent } from "@testing-library/react";
+import Sublist from "../sublist";
+
+afterEach(() => {
+  cleanup();
+});
+
+it("Renders main item name propely", () => {
+  render(<Sublist mainItemName="Correct" sublistItemNames={["Ok","Alright"]} />);
+  const element = screen.getByText(/Correct/i);
+  expect(element).toBeInTheDocument();
+});
+
+it("Renders sublist propely", () => {
+  render(<Sublist mainItemName="Correct" sublistItemNames={["Ok","Alright"]} />);
+  fireEvent.click(screen.getByText(/Correct/i));
+  const element = screen.getByText(/Ok/i);
+  expect(element).toBeInTheDocument();
+  const elementTwo = screen.getByText(/Alright/i);
+  expect(elementTwo).toBeInTheDocument();
+});
+
